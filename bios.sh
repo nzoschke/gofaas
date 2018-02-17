9ec96c2ed54b880671fae3538ab6b9556f43b118
Noah Zoschke <noah@mixable.net>
2018-02-17 07:35:52 -0800

    go lint, vet, build, test

--- bios.sh
+++ bios.sh
@@ -5,4 +5,10 @@ run -s "Cloning"  git clone $URL --branch $REF --single-branch .
 git reset --hard $SHA
 
 export PYTHONPATH=.pip
-run -s "Linting" python3 .pip/yamllint -d "{extends: default, rules: {line-length: {max: 120}, key-ordering: {}}}" template.yml
+run -s "YAML Linting" python3 .pip/yamllint -d "{extends: default, rules: {line-length: {max: 120}, key-ordering: {}}}" template.yml
+
+PKGS=$(go list $PKG/...)
+run -s "Linting"  golint -set_exit_status $PKGS
+run -s "Vetting"  go vet -x $PKGS
+run -s "Building" go build -v $PKGS
+run -s "Testing"  go test -v $PKGS

c0242b23a0eca720b7cb0616632d42c36bd6d611
Noah Zoschke <noah@mixable.net>
2018-02-17 08:55:41 -0800

    stack trace pattern

--- worker.go
+++ worker.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -25,7 +26,7 @@ func Worker(ctx context.Context, e WorkerEvent) error {
 
 	b, err := json.Marshal(e)
 	if err != nil {
-		return err
+		return errors.WithStack(err)
 	}
 
 	_, err = S3().PutObjectWithContext(ctx, &s3.PutObjectInput{
@@ -33,5 +34,5 @@ func Worker(ctx context.Context, e WorkerEvent) error {
 		Bucket: aws.String(os.Getenv("BUCKET")),
 		Key:    aws.String(uuid.NewV4().String()),
 	})
-	return err
+	return errors.WithStack(err)
 }

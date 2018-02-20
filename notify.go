72692bac61ad51ca2aca9c525fe96e2c4f88d3cb
Noah Zoschke <noah@mixable.net>
2018-02-19 20:39:03 -0800

    notify middlewares

---
+++ notify.go
@@ -0,0 +1,68 @@
+package gofaas
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sns"
+)
+
+// HandlerAPIGateway is an API Gateway Proxy Request handler function
+type HandlerAPIGateway func(ctx context.Context, e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error)
+
+// HandlerCloudWatch is a CloudWatchEvent handler function
+type HandlerCloudWatch func(ctx context.Context, e events.CloudWatchEvent) error
+
+// HandlerWorker is a Worker handler function
+type HandlerWorker func(ctx context.Context, e WorkerEvent) error
+
+// NotifyAPIGateway wraps a handler func and sends an SNS notification on error
+func NotifyAPIGateway(h HandlerAPIGateway) HandlerAPIGateway {
+	return func(ctx context.Context, e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		r, err := h(ctx, e)
+		notify(ctx, err)
+		return r, err
+	}
+}
+
+// NotifyCloudWatch wraps a handler func and sends an SNS notification on error
+func NotifyCloudWatch(h HandlerCloudWatch) HandlerCloudWatch {
+	return func(ctx context.Context, e events.CloudWatchEvent) error {
+		err := h(ctx, e)
+		notify(ctx, err)
+		return err
+	}
+}
+
+// NotifyWorker wraps a handler func and sends an SNS notification on error
+func NotifyWorker(h HandlerWorker) HandlerWorker {
+	return func(ctx context.Context, e WorkerEvent) error {
+		err := h(ctx, e)
+		notify(ctx, err)
+		return err
+	}
+}
+
+func notify(ctx context.Context, err error) {
+	topic := os.Getenv("NOTIFICATION_TOPIC")
+
+	if err == nil || topic == "" {
+		return
+	}
+
+	subj := fmt.Sprintf("ERROR %s", os.Getenv("AWS_LAMBDA_FUNCTION_NAME"))
+	msg := fmt.Sprintf("%+v\n", err)
+
+	_, err = SNS().PublishWithContext(ctx, &sns.PublishInput{
+		Message:  aws.String(msg),
+		Subject:  aws.String(subj),
+		TopicArn: aws.String(topic),
+	})
+	if err != nil {
+		log.Printf("NotifyError SNS Publish error %+v\n", err)
+	}
+}

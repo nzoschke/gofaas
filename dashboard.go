2e9123d2512e20e5a7024c03fb93e3e7c7568978
Noah Zoschke <noah@mixable.net>
2018-02-21 07:12:54 -0800

    dashboard response

--- dashboard.go
+++ dashboard.go
@@ -9,7 +9,7 @@ import (
 // Dashboard returns a dashboard HTML page
 func Dashboard(ctx context.Context, e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
-		Body: string("<html><body><h1>GoFAAS Dashboard</h1></body></html>"),
+		Body: string("<html><body><h1>gofaas dashboard</h1></body></html>\n"),
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},

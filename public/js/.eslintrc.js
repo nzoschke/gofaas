0321ab9941c59871c609d772db4fce6d79fae60d
Noah Zoschke <noah@mixable.net>
2018-03-18 13:03:03 -0700

    static app uses JWT claims and auth

---
+++ public/js/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "no-console": 0,
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};

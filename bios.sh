ac3276d37aa586ad6a8c5178819044a5f4d2f9d0
Noah Zoschke <noah@mixable.net>
2018-10-09 06:18:47 -0700

    separate get step, dont make in parallel

--- bios.sh
+++ bios.sh
@@ -7,7 +7,8 @@ git reset --hard $SHA
 
 run -s "YAML Linting" yamllint -d "{extends: default, rules: {line-length: {max: 140}, key-ordering: {}}}" template.yml
 
+run -s "Getting"  go get
 run -s "Linting"  golint -set_exit_status ./...
 run -s "Vetting"  go vet ./...
-run -s "Making"   make -j handlers-go
+run -s "Making"   make handlers-go
 run -s "Testing"  go test -v ./...
